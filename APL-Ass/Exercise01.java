8946b658d82582b252bf2c4bdd25d05d69aff1fb
SIPHAI <phalsiphai18@gmail.com>
2023-03-12 22:39:03 +0700

    Exercise01

--- APL-Ass/Exercise01.java
+++ APL-Ass/Exercise01.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
+import java.util.Scanner; // import the Scanner class 
 
-public class  Exercise01 {
-    static void main(String [] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your name:");
-    }
+class Exercise1 {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Enter username and press Enter
+    System.out.println("Enter Your Name:"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Hello: " + userName);        
+  }
 }
