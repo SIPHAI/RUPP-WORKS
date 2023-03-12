8c02a3594fec9ccce2d4944d3bdfbf8dbcc0061c
SIPHAI <phalsiphai18@gmail.com>
2023-03-12 22:34:31 +0700

    commit

---
+++ APL-Ass/Exercise01.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class  Exercise01 {
+    static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+    }
+}
