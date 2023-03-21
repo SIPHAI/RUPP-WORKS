d02e3ce45224ae57c23be2d26ad91a27460a1fcc
SIPHAI <phalsiphai18@gmail.com>
2023-03-21 09:34:07 +0700

    commit

--- web/run.py
+++ web/run.py
@@ -1,22 +1,24 @@
-from flask import Flask ,render_template
-app = Flask(__name__,template_folder = 'template')
-app.debug = True
+# from flask import Flask ,render_template
+# app = Flask(__name__,template_folder = 'template')
+# app.debug = True
 
+# @app.route("/")
+# def helloworld():
+#     return "<p> Hello, World!</p>"
+# @app.route("/home")
+# def helloworld():
+#     return render_template('home.html')
+# if __name__ == '__main__':
+#     app.run(host = 'localhost', port = 8000)
+    
+    
+from flask import Flask,render_template
+app=Flask(__name__,template_folder='templates')
+app.debug = True
 @app.route("/")
 def helloworld():
-    return "<p> Hello, World!</p>"
+    return "<p>Hello, World!</p>"
 @app.route("/home")
-def helloworld():
-    return render_template('home.html')
+def home():    return render_template('home.html')
 if __name__ == '__main__':
-    app.run(host = 'localhost', port = 8000)
-    
-    
-    # from flask import Flask,render_template
-    # app=Flask(__name__,template_folder='templates')
-    # app.debug = True@app.route("/")
-    # def helloworld():
-    #     return "<p>Hello, World!</p>"@app.route("/home")
-    # def home():    return render_template('home.html')
-    # if __name__ == '__main__':
-    #     app.run(host='localhost', port=8080)
+    app.run(host='localhost', port=8080)
