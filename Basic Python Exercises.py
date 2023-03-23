0252661c8f3da630d362a567f0a3528c9fb03594
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2023-03-23 17:31:17 +0700

    Add files via upload

---
+++ Basic Python Exercises.py
@@ -0,0 +1,3 @@
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are!,\n\t   Up above the world so hight\n\t     Like a diamond in the sky :")
+# For this Ex I have learned that to print sting in Python we have just use (,) and to get new line we use \n and \t for spaces or we can type space space to get space
