def217fab3645b492a9d4fb972cde4c8343b36b5
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2023-03-23 17:30:44 +0700

    Add files via upload

---
+++ Multifly.py
@@ -0,0 +1,11 @@
+# Python program to multiply three number
+
+A = int(input("Enter the first num: "));
+B = int(input("Enter the second num: "));
+C = int(input("Enter the third num: "));
+
+# calculate product
+Multiply = A * B * C
+
+# print multiplication value
+print("The Result of multiply is :", Multiply)
